2a21b4f494103b730ca3e7a45965268f8aa7861c
omar3o3 <otantawy53@gmail.com>
2022-05-19 12:33:33 -0400

    commit tree 32c31ca0f2e1cb0f0e4041295d63af96a9bf69a7

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [bttnState , setBttn] = useState(false);
+  const [clsState , setCls] = useState(false);
+
+  const addToCart = () =>{
+    setCls((clsState) => !clsState)
+    setBttn((bttnState) => !bttnState)
+  }
+
   return (
-    <li className="">
+    <li className={clsState ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className = {bttnState ? 'remove' : 'add'} onClick={addToCart}>
+        {bttnState ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory , setCategory] = useState('All')
+  
+  const updateCategoryState = (event) => {
+    let newCategory = event.target.value;
+    setCategory((selectedCategory) => selectedCategory = newCategory);
+  }
+
+  const handleFilter = items.filter(item => {
+    if (selectedCategory === 'All'){
+      return item
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={updateCategoryState}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {handleFilter.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
